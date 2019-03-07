e9d327c65e5546fb26827912dfe94a173954bc66
Jack Steam <jacksteamdev@gmail.com>
2019-03-07 12:45:06 -0600

    Add minimum rollup.watch tests

--- tests/src/index.test.js
+++ tests/src/index.test.js
@@ -1,13 +1,53 @@
-import { rollup } from 'rollup'
+import { rollup, watch } from 'rollup'
 import generateCode from '../../src/generateCode'
-import config from '../rollup.config'
+import config from '../rollup.test.config'
 
 describe('build', () => {
-  test('imports a file', async () => {
+  test('returns a string', async () => {
     const bundle = await rollup(config)
 
     const code = await generateCode(bundle, config)
 
-    expect(code).toBeDefined()
+    expect(typeof code).toBe('string')
+  })
+
+  test('bundles all imports', async () => {
+    const bundle = await rollup(config)
+
+    const code = await generateCode(bundle, config)
+
+    expect(code).toContain('const add')
+    expect(code).toContain('const b')
+    expect(code).toContain('console.log(\'c\')')
+    expect(code).toContain('const codeAsString')
+  })
+})
+
+describe('watch', () => {
+  test.only('successfully builds', done => {
+    const watcher = watch(config)
+    const spy = jest.fn()
+
+    const expectations = event => {
+      if (event.code === 'END') {
+        watcher.close()
+        done()
+      }
+    }
+
+    watcher.on('event', spy)
+    watcher.on('event', event => {
+      switch (event.code) {
+        case 'START':
+        case 'BUNDLE_START':
+        case 'BUNDLE_END':
+          return
+        case 'END':
+          return expectations(event)
+        case 'ERROR':
+        case 'FATAL':
+          throw new Error(event.code)
+      }
+    })
   })
 })
