6204d22bb9e9bb834069789370a4673599e1b126
Jack Steam <jacksteamdev@gmail.com>
2019-03-07 12:42:46 -0600

    Delete fixtures/dest

--- tests/fixtures/dest/entry-esm.js
+++
@@ -1,9 +0,0 @@
-var stringCode = "(function () {\n\t'use strict';\n\n\tconst b = 'B';\n\n\tconsole.log('c');\n\n\tconsole.log(b);\n\n}());\n";
-
-const add = (x, y) => {
-  return x + y
-};
-
-console.log(stringCode);
-
-export { add };
