06502247efe4b9ba93f2937abe1fcedabff6fb78
Jack Steam <teamsteamdev@gmail.com>
2019-04-25 16:33:26 -0500

    Refactor into rollup-plugin-bundle-import

--- README.md
+++ README.md
@@ -1,21 +1,86 @@
-# rollup-plugin-code-string
+# rollup-plugin-bundle-import
 
-Import javascript modules as strings.
+Bundle an import separately and use the result in your code.
 
 ## Installation
 
 ```sh
-npm i rollup-plugin-code-string -D
+npm i rollup-plugin-bundle-import -D
 ```
 
 ## Usage
 
+## Bundle a service worker
+
+`rollup.config.js`:
+
 ```js
 import { rollup } from 'rollup'
-import code from 'rollup-plugin-code-string'
+import bundle from 'rollup-plugin-bundle-import'
 
 rollup({
-  entry: 'main.js',
-  plugins: [code()]
+  entry: 'register-service-worker.js',
+  plugins: [
+    bundle({
+      include: ['**/my-sw.js'],
+      // Import as path to bundle
+      importAs: 'path',
+    }),
+  ],
 })
+```
+
+`register-service-worker.js`:
+
+```js
+import sw from './my-sw.js'
+
+navigator.serviceWorker.register(sw)
+```
+
+## Bundle a Web Extension content script
+
+Bundle a content script to a code string to inject from the background page of a Web or Chrome Extension.
+
+`rollup.config.js`
+
+```js
+import { rollup } from 'rollup'
+import bundle from 'rollup-plugin-bundle-import'
+
+rollup({
+  entry: 'background.js',
+  plugins: [
+    bundle({
+      include: ['**/content.js', '**/inject.js'],
+      // Import as code string to bundle
+      importAs: 'code',
+    }),
+  ],
+})
+```
+
+`background.js`
+
+```js
+import code from './content.js'
+
+// Inject the bundled code to the active tab
+browser.tabs.executeScript({ code })
+```
+
+## Recursive Usage
+
+Inject the bundled code from the content script of the previous example into the page runtime through a script tag.
+
+`content.js`
+
+```js
+import code from './inject.js'
+
+const script = document.createElement('script')
+script.text = code
+
+document.head.append(script)
+script.remove()
 ```
--- src/index.js
+++ src/index.js
@@ -1,59 +1,109 @@
 import { createFilter } from 'rollup-pluginutils'
 import { rollup } from 'rollup'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
+import path from 'path'
 
 import generateCode from './generateCode'
 
-function codeString({
-  include = ['**/*.code.js'],
-  exclude = [],
-  plugins = [resolve(), commonjs()],
-  output = {
-    format: 'iife',
-    preferConst: true,
-  },
+const name = 'bundle-import'
+
+function bundleImport({
+  include,
+  exclude,
+  importAs,
+  options: {
+    plugins,
+    output = {
+      format: 'esm',
+      preferConst: true,
+    },
+  } = {},
+  ...inputOptions
 } = {}) {
+  if (!include) {
+    throw new TypeError('options.include must be defined.')
+  }
+
+  if (!importAs) {
+    throw new TypeError('options.importAs must be defined.')
+  }
+
+  if (!output.format) {
+    throw new TypeError('options.options.format must be defined')
+  }
+
   const filter = createFilter(include, exclude)
 
   // Convert to arrays
-  exclude = [].concat(exclude)
-  include = [].concat(include)
+  const _include = [].concat(include)
+  const _exclude = exclude ? [].concat(exclude) : []
+  let _plugins = []
 
   return {
-    name: 'code-string',
+    name,
+
+    options({ plugins: p }) {
+      _plugins = plugins || p.filter(({ name: n }) => n !== name)
+    },
 
     async load(id) {
       if (!filter(id)) return null
 
-      try {
-        const bundle = await rollup({
-          input: id,
-          // Should exclude the current module in recursive bundles
-          plugins: [
-            ...plugins,
-            codeString({ exclude: exclude.concat(id) }),
-          ],
-        })
+      // try {
+      const bundle = await rollup({
+        input: id,
+        // Should exclude the current module in recursive bundles
+        plugins: _plugins.concat(
+          bundleImport({
+            include: _include,
+            exclude: _exclude.concat(id),
+            importAs,
+            options: {
+              plugins: _plugins,
+              output,
+              ...inputOptions,
+            },
+          }),
+        ),
+        ...inputOptions,
+      })
 
-        const code = await generateCode(bundle, {
-          input: id,
-          output,
-        })
+      bundle.watchFiles.forEach(file => {
+        this.addWatchFile(file)
+      })
 
-        bundle.watchFiles.forEach(file => {
-          this.addWatchFile(file)
-        })
+      const code = await generateCode(bundle, {
+        input: id,
+        output,
+      })
 
-        return {
-          code: `export default ${JSON.stringify(code)};`,
-          map: { mappings: '' },
+      switch (importAs) {
+        case 'code': {
+          return {
+            code: `export default ${JSON.stringify(code)};`,
+            map: { mappings: '' },
+          }
         }
-      } catch (error) {
-        this.error(error)
+
+        case 'path': {
+          const assetId = this.emitAsset(path.basename(id), code)
+
+          return {
+            code: `export default import.meta.ROLLUP_ASSET_URL_${assetId}`,
+            map: { mappings: '' },
+          }
+        }
+
+        default: {
+          throw new TypeError(
+            `Unknown options.importAs: '${importAs}'`,
+          )
+        }
       }
+      // } catch (error) {
+      //   this.error(error)
+      // }
     },
   }
 }
 
-export default codeString
+export default bundleImport
